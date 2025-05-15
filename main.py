bd37a220dcdc4f8c686305d94f03e0804c7c981d
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-14 23:41:12 -0400

    Refactor main.py to implement a 4x4 maze grid, replacing the previous 2x2 cell structure and integrating the Maze class for cell management and visualization.

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
 from tkinter import Tk, BOTH, Canvas
-from line import Line
-from point import Point
+from maze import Maze
 from window import Window
-from cell import Cell
 
 def main():
     win = Window(800, 600)
     
-    # Create a 2x2 grid of cells
-    cell1 = Cell(win)
-    cell1.draw(50, 50, 100, 100)
-    
-    cell2 = Cell(win)
-    cell2.draw(100, 50, 150, 100)
-    
-    cell3 = Cell(win)
-    cell3.draw(50, 100, 100, 150)
-    
-    cell4 = Cell(win)
-    cell4.draw(100, 100, 150, 150)
-    
-    # Draw some moves between cells
-    # Regular move from cell1 to cell2
-    cell1.draw_move(cell2)
-    
-    # Regular move from cell2 to cell4
-    cell2.draw_move(cell4)
-    
-    # Backtrack move from cell4 to cell3
-    cell4.draw_move(cell3, undo=True)
+    # Create a maze with a 4x4 grid of cells
+    # Starting at (50,50)
+    # Each cell 50x50 pixels
+    maze = Maze(50, 50, 4, 4, 50, 50, win)
     
     win.wait_for_close()
 
