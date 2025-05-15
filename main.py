840098988b530af059b36230cecb19d109c383e0
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-14 23:27:49 -0400

    Add main application window using Tkinter

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Maze Runner")
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
+
+    
